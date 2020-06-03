b2140bc1a830561762ef232c0348bf93f02f6b0f
Kai-Uwe Hermann <khermann@fzi.de>
2020-06-03 15:37:34 +0200

    make python 2/3 compatible

--- src/roswww/utils.py
+++ src/roswww/utils.py
@@ -54,7 +54,7 @@ def run_shellcommand(*args):
     '''run the provided command and return its stdout'''
     args = sum([(arg if type(arg) == list else [arg]) for arg in args], [])
     return subprocess.Popen(args,
-                            stdout=subprocess.PIPE).communicate()[0].strip()
+                            stdout=subprocess.PIPE).communicate()[0].decode('utf-8').strip()
 
 def split_words(text):
     '''return a list of lines where each line is a list of words'''
