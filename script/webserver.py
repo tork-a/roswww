6c1845f617d943c506f3012257eeaf137895319b
Shingo Kitagawa <shingogo@hotmail.co.jp>
2020-07-15 02:27:24 +0900

    add basic and basic_yaml in webserver

--- script/webserver.py
+++ script/webserver.py
@@ -48,17 +48,21 @@ def parse_argument(argv):
     parser.add_argument('-p', '--port', default=80, type=int, help='Webserver Port number')
     parser.add_argument('-w', '--webpath', default='www', help='package relative path to web pages')
     parser.add_argument('--cached', default='true', help='static file is cached')
+    parser.add_argument('--basic', default='false', help='enable basic authentication')
+    parser.add_argument('--basic-yaml', default=None, help='basic key yaml file path')
     parser.add_argument('--start_port', default=8000, type=int, help='setting up port scan range')
     parser.add_argument('--end_port', default=9000, type=int, help='setting up port scan range')
 
     parsed_args = parser.parse_args(argv)
     cached = False if parsed_args.cached in [0, False, 'false', 'False'] else True
-    return parsed_args.name, parsed_args.webpath, (parsed_args.port, parsed_args.start_port, parsed_args.end_port), cached
+    basic = True if parsed_args.basic in [1, True, 'true', 'True'] else False
+    basic_yaml = parsed_args.basic_yaml
+    return parsed_args.name, parsed_args.webpath, (parsed_args.port, parsed_args.start_port, parsed_args.end_port), cached, basic, basic_yaml
 
 
 if __name__ == '__main__':
     rospy.init_node("webserver", disable_signals=True)
-    name, webpath, port, cached = parse_argument(rospy.myargv()[1:])
-    webserver = roswww.ROSWWWServer(name, webpath, port, cached)
+    name, webpath, port, cached, basic, basic_yaml = parse_argument(rospy.myargv()[1:])
+    webserver = roswww.ROSWWWServer(name, webpath, port, cached, basic, basic_yaml)
     webserver.loginfo("Initialised")
     webserver.spin()
