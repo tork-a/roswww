01c7e4005554c01f03f58b2dc12959551e6cdd80
Kei Okada <k-okada@jsk.t.u-tokyo.ac.jp>
2020-12-02 10:42:05 +0000

    python3 returns bytes in subproces.Popen.communicate
    c.f. https://stackoverflow.com/questions/33054527/typeerror-a-bytes-like-object-is-required-not-str-when-writing-to-a-file-in
         https://stackoverflow.com/questions/15374211/why-does-popen-communicate-return-bhi-n-instead-of-hi

--- src/roswww/utils.py
+++ src/roswww/utils.py
@@ -53,8 +53,12 @@ def get_packages():
 def run_shellcommand(*args):
     '''run the provided command and return its stdout'''
     args = sum([(arg if type(arg) == list else [arg]) for arg in args], [])
-    return subprocess.Popen(args,
+    text = subprocess.Popen(args,
                             stdout=subprocess.PIPE).communicate()[0].strip()
+    # if python3
+    if not isinstance(text, str):
+        text = text.decode('ascii')
+    return text
 
 def split_words(text):
     '''return a list of lines where each line is a list of words'''
